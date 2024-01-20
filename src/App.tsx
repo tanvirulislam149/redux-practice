69eaadbc1d9679f0b4b83dbdf129491109405b1d
tanvir <tanvirulislam149@gmail.com>
2024-01-20 21:34:19 +0600

    basics done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { RootState } from "./redux/store";
+import {
+	decrement,
+	increment,
+	incrementByValue,
+} from "./redux/features/counter/counterSlice";
 
 function App() {
+	const count = useSelector((state: RootState) => state.counter.count);
+
+	const dispatch = useDispatch();
+
 	return (
-		<>
-			<h1 className="text-3xl font-bold">Checking</h1>
-		</>
+		<div className="flex justify-center items-center h-screen">
+			<div>
+				<p>{count}</p>
+				<button
+					onClick={() => dispatch(increment())}
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+				>
+					Increment
+				</button>
+				<button
+					onClick={() => dispatch(decrement())}
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+				>
+					Decrement
+				</button>
+				<button
+					onClick={() => dispatch(incrementByValue(5))}
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+				>
+					Increment by value
+				</button>
+			</div>
+		</div>
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import App from "./App.tsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Login from "./page/Login.tsx";
+import NotFound from "./page/NotFound.tsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 
 const router = createBrowserRouter([
 	{
@@ -14,10 +17,16 @@ const router = createBrowserRouter([
 		path: "/login",
 		element: <Login />,
 	},
+	{
+		path: "*",
+		element: <NotFound />,
+	},
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
 	</React.StrictMode>
 );
